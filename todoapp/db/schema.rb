17d409df8e14f9fd3f97cad5082119e10bb6762a
nmtuan2001 <nmtuan2001@gmail.com>
2020-12-23 20:33:44 +0800

    Add new gems, create Task model

--- todoapp/db/schema.rb
+++ todoapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_22_143242) do
+ActiveRecord::Schema.define(version: 2020_12_23_112359) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
