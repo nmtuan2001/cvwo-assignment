b45fafb090a4d80569035c487cef56001748cb37
nmtuan2001 <nmtuan2001@gmail.com>
2020-12-29 11:31:59 +0800

    Add completed/uncompleted

--- todomanager/app/controllers/main_controller.rb
+++ todomanager/app/controllers/main_controller.rb
@@ -1,4 +1,11 @@
 class MainController < ApplicationController
   def index
   end
+
+  def all_tasks
+  	completed = Task.where(completed: true)
+  	uncompleted = Task.where(completed: false)
+  	render json: { completed: completed, uncompleted: uncompleted }
+  end
+
 end
--- todomanager/db/seeds.rb
+++ todomanager/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |index|
+  Task.create!({ title: "Task #{index + 1}", completed: false})
+end
+
+puts "5 uncompleted tasks created"
+
+5.times do |index|
+  Task.create!({ title: "Task #{index + 1}", completed: true})
+end
+
+puts "5 completed tasks created"
